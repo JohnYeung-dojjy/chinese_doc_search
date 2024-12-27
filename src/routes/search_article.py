da6a9934f63187eb4a2f51368837fe72b4ee8037
John Dojjy Yeung <p80246@gmail.com>
2024-12-27 16:54:22 +0800

    Article Search - add perPage setting btns

--- src/routes/search_article.py
+++ src/routes/search_article.py
@@ -18,6 +18,47 @@ HIGHLIGHT_SETTINGS = HighlightSettings(
     segment_token=" <b>...</b> "
 )
 
+PER_PAGE_OPTIONS = [10, 20, 50]
+
+BTN_ACTIVATED_CLS = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+BTN_DEACTIVATED_CLS = "bg-blue-200 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+
+FORM_SUBMISSION_HTMX_KW = dict(
+    method="post",
+    hx_post="/search-article",
+    hx_target="#search_result_table",
+    hx_swap="innerHTML",
+)
+
+DEFAULT_DISPLAY_ROWS: int = 10
+
+PAGINATION_SETTING_JS = """
+    let perPage = 10;
+    let pageNum = 0;
+
+    /* Must use function(){} else $(this) is the same as outer scope */
+    $("[id^=id_per_page-]").on("click", function() {
+        /* Get the first page of the updated query */
+        let shouldUpdatePage = $(this).val() !== perPage;
+        if (!shouldUpdatePage) { return; }
+
+        perPage = $(this).val();
+        pageNum = 0;
+
+        /* Update btn ui */
+        $("[id^=id_per_page-]").each(function() {
+            if ($(this).val() === perPage) {
+                $(this).removeClass("bg-blue-200").addClass("bg-blue-500");
+            } else {
+                $(this).removeClass("bg-blue-500").addClass("bg-blue-200");
+            }
+        })
+
+        /* Resubmit the form */
+        $("#search_result_table").submit();
+    })
+"""
+
 SearchLabel = partial(Label, cls=[
     # "block",
     "mb-2",
@@ -48,10 +89,10 @@ SearchInput = partial(Input, cls=[
 ])
 
 article_search_form = Form(
-    method="post",
-    hx_post="/search-article",
-    hx_target="#search_result_table",
-    hx_swap="innerHTML",
+    id="article_search_form",
+    onclick="perNum=0",
+    hx_vals=r"js:{per_page: perPage, page_id: pageNum}",
+    **FORM_SUBMISSION_HTMX_KW,
 )(
     Fieldset(
         Div(
@@ -71,10 +112,35 @@ article_search_form = Form(
         ]
     ),
     Div(
+        Div(
+            Label(
+                "Display Rows",
+                cls=[
+                    "mb-2",
+                    "text-sm",
+                    "font-medium",
+                    "text-gray-900",
+                    "p-2",
+                ]
+            ),
+            Span(
+                *[Button(
+                        val,
+                        type="submit",
+                        id=f"id_per_page-{val}",
+                        value=val,
+                        cls=BTN_ACTIVATED_CLS if val == DEFAULT_DISPLAY_ROWS else BTN_DEACTIVATED_CLS
+                    ) for val in PER_PAGE_OPTIONS],
+                Script(PAGINATION_SETTING_JS),
+            ),
+            cls=[
+                # "flex",
+            ]
+        ),
         Button("Search", type="submit",
             cls="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
         ),
-        cls="justify-self-end"
+        cls="flex justify-between"
     ),
     cls=[
         "justify-self-auto",
@@ -206,8 +272,8 @@ def search_article(
     author_name: str,
     title: str,
     full_text: str,
-    per_page: int = 10,
-    page_id: int = 0,
+    per_page: int,
+    page_id: int,
 ):
     """Search article documents in elasticsearch with the given keywords"""
     if not any([publisher, publish_location, publish_date_start, publish_date_end, author_name, title, full_text]):
